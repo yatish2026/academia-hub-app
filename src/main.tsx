57b45472d5aa4630677d41a81f42edc1eedd2f06
Yatish Gottapu <yatishgottapu2026@gmail.com>
2026-05-01 23:56:29 +0530

    Convert project to static SPA for Vercel deployment

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const queryClient = new QueryClient();
+const router = getRouter();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
